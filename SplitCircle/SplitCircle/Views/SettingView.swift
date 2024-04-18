910927cfd64c2ce502dfb755de544a9df4fe465d
Steven Hu <78782412+shstevenhu@users.noreply.github.com>
2024-04-17 23:30:03 -0400

    Add Bar Tab

---
+++ SplitCircle/SplitCircle/Views/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  SplitCircle
+//
+//  Created by Steven Hu on 4/17/24.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text("Setting View")
+    }
+}
+
+#Preview {
+    SettingView()
+}
