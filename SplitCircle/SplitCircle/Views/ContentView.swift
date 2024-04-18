b6c5856b8f0201cbb6b45128d3ba7f17e74a4141
XIANGYU KONG <xiangyukongdeveloper@gmail.com>
2024-04-17 21:05:10 -0400

    clean up

--- SplitCircle/SplitCircle/Views/ContentView.swift
+++ SplitCircle/SplitCircle/Views/ContentView.swift
@@ -9,54 +9,11 @@ import SwiftData
 import SwiftUI
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var users: [User]
-
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(users) { user in
-                    NavigationLink {
-                        Text("User at \(user.email)")
-                    } label: {
-                        Text("hi")
-                        Text(user.name)
-                    }
-                }
-                .onDelete(perform: deleteUsers)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addUser) {
-                        Label("Add User", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-    }
-
-    private func addUser() {
-        withAnimation {
-            let newUser = User(id: UUID().uuidString, email: "test@g.com", name: "xiangyu")
-            modelContext.insert(newUser)
-        }
-    }
-
-    private func deleteUsers(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(users[index])
-            }
-        }
+        Text("Hello Content View")
     }
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: User.self, inMemory: true)
 }
