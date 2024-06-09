c76dc656fc9a6a2a618d71c167b248bb48c0c525
XIANGYU KONG <xiangyukongdeveloper@gmail.com>
2024-06-09 10:49:41 -0400

    move swiftdata models to versioned schema

---
+++ SplitCircle/SplitCircle/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  SplitCircle
+//
+//  Created by XIANGYU KONG on 6/9/24.
+//
+
+import Foundation
+
+typealias User = SplitCircleSchemaV1.User
+typealias Expense = SplitCircleSchemaV1.Expense
+typealias MemberGroup = SplitCircleSchemaV1.MemberGroup
+typealias ExpenseTransaction = SplitCircleSchemaV1.ExpenseTransaction
